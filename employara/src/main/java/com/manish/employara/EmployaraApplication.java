b0f4a80484c7f7d813f7b8210e85c8f45c237087
mukhiyamanish803 <mukhiyamanish803@gmail.com>
2025-06-23 11:59:01 +0530

    Add files via upload

---
+++ employara/src/main/java/com/manish/employara/EmployaraApplication.java
@@ -0,0 +1,13 @@
+package com.manish.employara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployaraApplication.class, args);
+	}
+
+}
