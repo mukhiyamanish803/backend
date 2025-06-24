ce677c95f0a0f925b0984cb505ce951a409ffb49
Manish Mukhiya <mukhiyamanish803@gmail.com>
2025-06-24 14:49:22 +0530

    Update DialCodeDTO.java

--- employara/src/main/java/com/manish/employara/dto/DialCodeDTO.java
+++ employara/src/main/java/com/manish/employara/dto/DialCodeDTO.java
@@ -1,6 +1,6 @@
 package com.manish.employara.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,12 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DialCodeDTO {
-    @NotNull(message = "Country name is required.")
+    @NotBlank(message = "Country name is required.")
     private String countryName;
 
+    @NotBlank(message = "Country code is required.")
     private String countryCode;
 
-    @NotNull(message = "Dial Code is required.")
+    @NotBlank(message = "Dial Code is required.")
     @Pattern(regexp = "^\\+\\d{1,4}$", message = "Dial Code must start with + followed by 1 to 4 digits.")
     private String dialCode;
 }
